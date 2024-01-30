a5563dd0e4a5ae11f585588a729227aedba2a8d2
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-01-29 23:04:06 -0700

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Page () {
+      return (
+        <main>
+          <h1>CPRG 306: WEB DEVELOPMENT 2 - Assignments</h1>
+          <Link href="week-2">Week 2 Assignments</Link>
+          </main>
+      );
+}
+
---
+++ week-2/student-info.js
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Gurleen</h1>
+      <Link href="https://github.com/Gurleen537/cprg306-assignments.git">https.//github.com</Link>
+    </main>
+  );
+}
