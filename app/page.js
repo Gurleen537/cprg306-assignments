f3f40f4a45f76f95141c6c26574fbcd08be57b79
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-01-31 21:38:27 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,8 +4,12 @@ export default function Page () {
       return (
         <main>
           <h1>CPRG 306: WEB DEVELOPMENT 2 - Assignments</h1>
+          <div>
           <Link href="week-2">Week 2 Assignments</Link>
+          </div>
+          <div>
           <Link href="week-3">Week 3 Assignments</Link>
+          </div>
           </main>
       );
 }
