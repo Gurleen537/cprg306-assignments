7faadd9eb97542690eb9f3e5b47b14a2988c2511
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-02-05 22:45:05 -0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,15 +3,24 @@ import Link from "next/link";
 export default function Page () {
       return (
         <main>
-          <h1>CPRG 306: WEB DEVELOPMENT 2 - Assignments</h1>
-          <div>
-          <Link href="week-2">Week 2 Assignments</Link>
-          </div>
-          <div>
-          <Link href="week-3">Week 3 Assignments</Link>
-          </div>
-          </main>
-      
-      );
-}
+          <h1 className="text-4xl font-bold pt-24 pl-56 pb-5">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
+      <Link
+        href="week-2"
+        className="text-xl pl-56 hover:text-sky-500 hover:underline"
+      >
+        Week 2 Assignment
+      </Link>
+      <br />
+      <Link
+        href="week-3"
+        className="text-xl pl-56 hover:text-sky-500 hover:underline"
+      >
+        Week 3 Assignment
+      </Link>
+      <br />
+      </main>
+);
+      }
 
