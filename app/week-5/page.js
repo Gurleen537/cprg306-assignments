5e8300bb0c88abbab24bf32db20645ed296a874d
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-02-26 21:50:55 -0700

    Update page.js

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,10 +1,10 @@
 import ItemList from "./item-list";
 export default function Page() {
   return (
-    <div>
-      <h1 className="m-3 text-black font-bold text-3xl ">Shopping List</h1>
+    <main className="bg-slate-950 text-white">
+      <h1 className="text-3xl m-2 font-bold text-gray-200 p-4">Shopping List</h1>
       <ItemList />
-    </div>
+    </main>
   );
 }
 
