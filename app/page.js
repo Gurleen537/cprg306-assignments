d273624184046b2e951ac7180694056d73696084
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-03-01 21:56:06 -0700

    page.js

--- app/page.js
+++ app/page.js
@@ -34,7 +34,14 @@ export default function Page() {
         Week 5 Assignment
       </Link>
       <br />
-    </main>
+      <Link
+           href="week-6"
+           className="text-xl pl-56 hover:text-sky-500 hover:underline"
+      >
+        Week 6 Assignment 
+      </Link>
+      <br />
+      </main>
 );
      }
 
