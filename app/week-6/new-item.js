b227fa986225ef601416133784506469a4f1b531
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-03-04 22:27:03 -0700

    Update new-item.js

--- app/week-6/new-item.js
+++ app/week-6/new-item.js
@@ -1,78 +1,78 @@
-export default function NewItem({ onAddItem }) {
-    const [name, setName] = useState("");
-    const [quantity, setQuantity] = useState(1);
-    const [category, setCategory] = useState("produce");
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      const item = { id: generateRandomId(), name, quantity, category };
-      onAddItem(item);
-      setName("");
-      setQuantity(1);
-      setCategory("produce");
-    }
-  
-    function generateRandomId() {
-      return Math.random().toString(36);
-    }
-  
-    return (
-      <div className="mb-2">
-        <form
-          className="p-2 m-4 bg-slate-500 text-black max-w-sm w-full"
-          onSubmit={handleSubmit}
-        >
-          <div className="mb-2">
-            <input
-              type="text"
-              id="name"
-              value={name}
-              placeholder="Item Name"
-              onChange={(event) => setName(event.target.value)}
-              required
-              className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
-            />
-          </div>
-          <div className="flex justify-between">
-            <input
-              type="number"
-              id="quantity"
-              value={quantity}
-              onChange={(event) => setQuantity(Number(event.target.value))}
-              min="1"
-              max="99"
-              required
-              className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
-            />
-            <select
-              id="category"
-              value={category}
-              onChange={(event) => setCategory(event.target.value)}
-              className="ml-1 border-2 borer-gray-300 p-2 rounded-lg font-sans"
-            >
-              <option value disabled>
-                Category
-              </option>
-              <option value="produce">Produce</option>
-              <option value="dairy">Dairy</option>
-              <option value="bakery">Bakery</option>
-              <option value="meat">Meat</option>
-              <option value="frozen">Frozen Foods</option>
-              <option value="canned">Canned Goods</option>
-              <option value="dry">Dry Goods</option>
-              <option value="beverages">Beverages</option>
-              <option value="snacks">Snacks</option>
-              <option value="household">Household</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <button
-            type="submit"
-            className="w-full mt-4 py-2 px-4 bg-blue-800 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+"use client";
+import { useState } from "react";
+
+export default function NewItem(onAddItem) {
+  const [name, setName] = useState(" ");
+  const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState("Produce");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const item = {
+      name,
+      quantity,
+      category,
+    };
+    // const newItem = { id, name, quantity, category };
+    // onAddItem(newItem);
+    onAddItem({ id, name, quantity, category });
+    setName("");
+    setQuantity(1);
+    setCategory("Produce");
+  };
+  const newId = Math.floor(Math.random() * 1000000);
+
+  return (
+    <main class="flex justify-center">
+      <form onSubmit={handleSubmit} className="m-4 p-4 bg-slate-800 text-black">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            placeholder="Item Name"
+            required
+            className="text-black m-4 inline-block border rounded-md font-medium p-3 shadow-xl pr-32"
+          />
+        </div>
+        <div className="flex justify-between">
+          <input
+            type="number"
+            value={quantity}
+            min="1"
+            max="99"
+            onChange={(event) => setQuantity(event.target.value)}
+            required
+            className="text-black m-4  inline-block border rounded-md font-medium p-3 shadow-xl"
+          />
+          <select
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+            className="text-black m-4  inline-block border rounded-md font-medium p-3 shadow-xl"
           >
-            +
-          </button>
-        </form>
-      </div>
-    );
-  }
+            <option value disabled>
+              Category
+            </option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Bakery">Bakery</option>
+            <option value="Meat">Meat</option>
+            <option value="Frozen Foods">Frozen Foods</option>
+            <option value="Canned Goods">Canned Goods</option>
+            <option value="Dry Goods">Dry Goods</option>
+            <option value="Beverages">Beverages</option>
+            <option value="Snacks">Snacks</option>
+            <option value="HouseHold">HouseHold</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-400 border pl-36 pr-36 flex p-1 rounded-md m-6"
+        >
+          +
+        </button>
+      </form>
+    </main>
+  );
+}
