96be96872a0b21e30337d17f5a3c13ba051fa01d
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-04-18 18:21:49 -0600

    Update page.js

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -4,7 +4,7 @@ import { useUserAuth } from "./_utils/auth-context";
 import Link from "next/link";
 
 export default function Page() {
-  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const { user, gitHubSignIn, firebaseSignOut } = getAnalytics();
 
   return (
     <div>
