63a6bb85e23ae29d9d18ed0e3749038f6c45a980
Gurleen Kaur <131569481+Gurleen537@users.noreply.github.com>
2024-04-19 01:33:31 -0600

    Update page.js

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -1,30 +1,21 @@
 "use client";
 
 import { useUserAuth } from "./_utils/auth-context";
-import Link from "next/link";
-
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
-
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-5">Shopping List App</h1>
-      <p className="text-lg">
+      <h1>Week-8</h1>
+      <p>{user ? "Hi" : "Please sign in"}</p>
+      <p>{user && user.displayName}</p>
+      <p>
         {user ? (
-          <>
-            <p>
-              Signed in as {user && user.displayName} ({user?.email})
-            </p>
-            <button onClick={firebaseSignOut}>Sign out</button>
-            <br />
-            <Link href="./week-8/shopping-list">
-              Continue to your Shopping List
-            </Link>
-          </>
+          <button onClick={firebaseSignOut}>Sign Out</button>
         ) : (
-          <button onClick={gitHubSignIn}>Sign in with GitHub</button>
+          <button onClick={gitHubSignIn}>Sign In with Github</button>
         )}
       </p>
     </div>
   );
 }
+
